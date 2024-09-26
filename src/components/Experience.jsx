2f445a580fdb13618e530f10bf72a90fe62d6897
Sanskar Gupta <129537566+Sanskar63@users.noreply.github.com>
2024-09-26 20:59:17 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,7 +5,7 @@ const Experience = () => {
     <div className='w-[80%] mx-auto flex flex-col gap-2'>
       <div className='flex gap-2 md:gap-4 lg:gap-6'>
         <span className='font-archivo-black text-xl md:text-2xl lg:text-3xl text-gray-800 dark:text-white'>Experience</span>
-        <a href="https://iiitu.me/devteam"><img className='w-7 md:w-10 hover:scale-[105%] cursor-pointer ' src="/link.png" alt="" /></a>
+        <a href="https://iiitu.ac.in/devteam"><img className='w-7 md:w-10 hover:scale-[105%] cursor-pointer ' src="/link.png" alt="" /></a>
       </div>
       <span className='w-[80%] font-mono text-sm md:text-xl lg:text-2xl text-gray-800 dark:text-white'>Worked as Backend Developer at Indian Institute of Information Technology.</span>
     </div>
