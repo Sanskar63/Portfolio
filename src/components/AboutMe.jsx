f94521beb25955221c30caaa37a7c2aea2541ea7
Sanskar Gupta <129537566+Sanskar63@users.noreply.github.com>
2024-09-27 16:13:00 +0530

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,8 +6,8 @@ const AboutMe = () => {
       <span className='font-archivo-black text-xl md:text-2xl lg:text-3xl text-gray-800 dark:text-white'>About Me</span>
       <p className='w-[80%] font-mono text-sm md:text-xl lg:text-2xl text-gray-800 dark:text-white'>
         Hello there, I am Sanskar. <br /> 
-        I love designing and developing websites and I'm good at it.<br />
-        I read non-fiction books, listen songs and watch Harry Potter Movies in my free time. <br />
+        I love designing and developing websites and I'm good at it. I can go on hours without stopping when it comes to developing. Having worked with my college's development team to re-build its website, I learned working with team and gained some experience.<br />
+        Other than web development, I read books, listen songs and watch Harry Potter Movies. <br />
 
       </p>
     </div>
